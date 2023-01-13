048ee6dd820d5f5ae051a91d069712c3b6b0262e
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-13 14:13:01 -0300

    Agrega el GET(equipos.json)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
 const express = require("express");
+const servicios = require("./servicios/servicios");
 
 const PUERTO = 8080;
 const app = express();
@@ -8,8 +8,21 @@ app.use(express.json());
 
 app.use(express.text({ type: "*/*" }));
 
-
+app.get("/", (req, res) => {
+  (async function () {
+    try {
+      const equiposCadena = await servicios.traerEquipos("datos/equipos.json");
+      const equiposObjeto = JSON.parse(equiposCadena);
+      res.end(JSON.stringify(equiposObjeto));
+    } catch (err) {
+      res.status(500);
+      res.end("Hubo un error en el servidor");
+      console.log("Hubo un error en el try del get: " + err);
+    }
+  })();
+});
 
 
 
 app.listen(PUERTO);
+console.log("Corriendo...");
