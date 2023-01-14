36db492ab47256e01d8fc4b6c6127fdecfca947c
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-14 17:00:49 -0300

    Quita el POST(equipos.json)
    Me di cuenta de que "equipos.json" solo necesita GET y DELETE.

--- index.js
+++ index.js
@@ -23,41 +23,5 @@ app.get("/equipos", (req, res) => {
   })();
 });
 
-app.post("/equipos", (req, res) => {
-  (async function () {
-    try {
-      const equipo = {
-        id: req.body.id,
-        area: req.body.area,
-        name: req.body.name,
-        shortName: req.body.shortName,
-        tla: req.body.tla,
-        crestUrl: req.body.crestUrl,
-        address: req.body.address,
-        phone: req.body.phone,
-        website: req.body.website,
-        email: req.body.email,
-        founded: req.body.founded,
-        clubColors: req.body.clubColors,
-        venue: req.body.venue,
-        lastUpdated: req.body.lastUpdated,
-      };
-
-      const equiposCadena = await servicios.traerEquipos("datos/equipos.json");
-      const equiposObjeto = JSON.parse(equiposCadena);
-      equiposObjeto.push(equipo);
-      await servicios.guardarEquipos(
-        "datos/equipos.json",
-        JSON.stringify(equiposObjeto)
-      );
-      res.end(JSON.stringify(equipo));
-    } catch (err) {
-      res.status(500);
-      res.end("Hubo un error en el servidor");
-      console.log("Hubo un error en el try del post: " + err);
-    }
-  })();
-});
-
 app.listen(PUERTO);
 console.log("Corriendo...");
