026a7b99f6167668e52ff4a5e6e9767202c130c4
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-14 19:56:02 -0300

    Agrega el POST(equipo.json)

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.get("/equipos", (req, res) => {
 app.get("/equipos/:tla", (req, res) => {
   (async function () {
     try {
-      const equiposCadena = await servicios.leerArchivo(`datos/equipos/${req.params.tla}.json`);
+      const equiposCadena = await servicios.leerArchivo(
+        `datos/equipos/${req.params.tla}.json`
+      );
       const equiposObjeto = JSON.parse(equiposCadena);
       res.end(JSON.stringify(equiposObjeto));
     } catch (err) {
@@ -36,6 +38,41 @@ app.get("/equipos/:tla", (req, res) => {
     }
   })();
 });
+
+app.post("/equipos/equipo", (req, res) => {
+  (async function () {
+    try {
+      const equipo = {
+        id: req.body.id,
+        area: req.body.area,
+        name: req.body.name,
+        shortName: req.body.shortName,
+        tla: req.body.tla,
+        crestUrl: req.body.crestUrl,
+        address: req.body.address,
+        phone: req.body.phone,
+        website: req.body.website,
+        email: req.body.email,
+        founded: req.body.founded,
+        clubColors: req.body.clubColors,
+        venue: req.body.venue,
+        lastUpdated: req.body.lastUpdated,
+      };
+
+      const equipoCadena = JSON.stringify(equipo);
+      await servicios.escribirArchivo(
+        `datos/equipos/${equipo.tla}.json`,
+        equipoCadena
+      );
+      res.end(equipoCadena);
+    } catch (err) {
+      res.status(500);
+      res.end("Hubo un error en el servidor");
+      console.log("Hubo un error en el try del post: " + err);
+    }
+  })();
+});
+
 
 app.listen(PUERTO);
 console.log("Corriendo...");
