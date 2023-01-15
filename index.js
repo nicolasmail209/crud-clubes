5d3dc682d5ff5ac8a411e0821e49728ecad3b946
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-15 12:25:11 -0300

    Agrega el PATCH(equipo.json)

--- index.js
+++ index.js
@@ -115,10 +115,62 @@ app.put("/equipos/equipo", (req, res) => {
     } catch (err) {
       res.status(500);
       res.end("Hubo un error en el servidor");
-      console.log("Hubo un error en el try del post: " + err);
+      console.log("Hubo un error en el try del put: " + err);
     }
   })();
 });
+
+
+app.patch("/equipos/equipo", (req, res) => {
+  (async function () {
+    try {
+      const equipo = {
+        id: req.body.id,
+        area: req.body.area,
+        activeCompetitions: req.body.activeCompetitions,
+        name: req.body.name,
+        shortName: req.body.shortName,
+        tla: req.body.tla,
+        crestUrl: req.body.crestUrl,
+        address: req.body.address,
+        phone: req.body.phone,
+        website: req.body.website,
+        email: req.body.email,
+        founded: req.body.founded,
+        clubColors: req.body.clubColors,
+        venue: req.body.venue,
+        squad: req.body.squad,
+        lastUpdated: req.body.lastUpdated,
+      };
+      
+      let equipoCadena = await servicios.leerArchivo(
+        `datos/equipos/${equipo.tla}.json`
+      );
+
+      const equipoObjeto = JSON.parse(equipoCadena);
+      for (const key in equipo) {
+        if(equipo[key] != undefined){
+        equipoObjeto[key] = equipo[key];
+        }
+        else{
+          equipoObjeto[key] = equipoObjeto[key];
+        }
+      }
+      equipoCadena = JSON.stringify(equipoObjeto);
+      await servicios.escribirArchivo(
+        `datos/equipos/${equipo.tla}.json`,
+        equipoCadena
+      );
+
+      res.end(equipoCadena);
+    } catch (err) {
+      res.status(500);
+      res.end("Hubo un error en el servidor");
+      console.log("Hubo un error en el try del patch: " + err);
+    }
+  })();
+});
+
 
 app.listen(PUERTO);
 console.log("Corriendo...");
