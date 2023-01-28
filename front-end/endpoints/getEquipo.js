0b504d32bbf79d6ed70ca0347b16bfae5f23cf3b
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-28 01:38:40 -0300

    Agrega la limpieza de patalla al "getEquipo()"

--- front-end/endpoints/getEquipo.js
+++ front-end/endpoints/getEquipo.js
@@ -1,6 +1,21 @@
 import { traerEquipo, extraerDatos, crearFormulario } from './getEquipoAyudantes.js';
 
 async function getEquipo(tla) {
+
+  function limpiarListaEquipo() {
+    const elementos = document.querySelectorAll("#listaEquipo li");
+    elementos.forEach((elemento) => {
+      elemento.remove();
+    });
+  }
+  function limpiarFormularioPost() {
+    const elementos = document.querySelectorAll("#formularioPost li");
+    elementos.forEach((elemento) => {
+      elemento.remove();
+    });
+  }
+  limpiarListaEquipo();
+  limpiarFormularioPost();
     const equipo = await traerEquipo(tla);
     const datosEquipo = extraerDatos(equipo);
     crearFormulario(datosEquipo)
