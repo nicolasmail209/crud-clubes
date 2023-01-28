63207dcfc6f422c65c378f6b9107823aeb1dd394
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-28 01:38:17 -0300

    Agrega la limpieza de pantalla al "postEquipo()"

--- front-end/endpoints/postEquipo.js
+++ front-end/endpoints/postEquipo.js
@@ -3,6 +3,20 @@ import { crearFormularioPost } from './postEquipoAyudantes.js';
 async function postEquipo(tla) {
     //const equipo = await traerEquipo(tla);
     //const datosEquipo = extraerDatos(equipo);
+    function limpiarListaEquipo() {
+      const elementos = document.querySelectorAll("#listaEquipo li");
+      elementos.forEach((elemento) => {
+        elemento.remove();
+      });
+    }
+    function limpiarFormularioPost() {
+      const elementos = document.querySelectorAll("#formularioPost li");
+      elementos.forEach((elemento) => {
+        elemento.remove();
+      });
+    }
+    limpiarListaEquipo();
+    limpiarFormularioPost();
     crearFormularioPost();
     
     
