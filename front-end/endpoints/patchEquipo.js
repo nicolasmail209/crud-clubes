0942af0578b843a9a3cd8a456ad8e3526aaf6ceb
nicolas nicolas <nicolasmail209@gmail.com>
2023-01-31 06:34:30 -0300

    Agrega "patchEquipo()"

--- front-end/endpoints/patchEquipo.js
+++ front-end/endpoints/patchEquipo.js
@@ -1,9 +1,28 @@
-import { traerEquipo, extraerDatos, crearFormulario } from './patchEquipoAyudantes.js';
+//import { traerEquipo, extraerDatos, crearFormulario } from './patchEquipoAyudantes.js';
+import { traerEquipo, extraerDatos } from './getEquipoAyudantes.js';
+import { cargarFormulario, crearFormularioPatch } from './patchEquipoAyudantes.js';
 
 async function patchEquipo(tla) {
+  function limpiarListaEquipo() {
+    const elementos = document.querySelectorAll("#listaEquipo li");
+    elementos.forEach((elemento) => {
+      elemento.remove();
+    });
+  }
+  function limpiarFormularioPost() {
+    const elementos = document.querySelectorAll("#formularioPost li");
+    elementos.forEach((elemento) => {
+      elemento.remove();
+    });
+  }
+  limpiarListaEquipo();
+  limpiarFormularioPost();
+
     const equipo = await traerEquipo(tla);
     const datosEquipo = extraerDatos(equipo);
-    crearFormulario(datosEquipo);
+    //crearFormulario(datosEquipo);
+    crearFormularioPatch();
+    cargarFormulario(datosEquipo);
     
 
     
